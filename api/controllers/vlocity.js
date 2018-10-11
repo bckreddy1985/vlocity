0ef29569edc7e057c8512ccef0f83d16aeed0a9a
Chandrakanthreddy Badvel <Chandrakanth_B02@ad.infosys.com>
2018-10-11 20:21:10 +1100

    first commit

---
+++ api/controllers/vlocity.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+module.exports.vlocityApi = function(req, res) {
+
+  if (!req.payload._id) {
+    res.status(401).json({
+      "message" : "UnauthorizedError: private profile"
+    });
+  } else {
+
+  }
+
+};
---
+++ api/routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('express-jwt');
+var auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload'
+});
+
+var ctrlProfile = require('../controllers/profile');
+var ctrlAuth = require('../controllers/authentication');
+var vlocityApi = require('../controllers/vlocity');
+
+// profile
+router.get('/profile', auth, ctrlProfile.profileRead);
+
+// authentication
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
+router.post('/vlocity',vlocityApi.vlocityApi);
+module.exports = router;
